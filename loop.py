9c47579e5e530a989995d95f841e3693df1ee18b
meshack34 <meshackkimutai34@gmail.com>
2024-02-07 16:40:11 -0500

    loops

---
+++ loop.py
@@ -0,0 +1,17 @@
+
+        
+arr = ["a", "b", "c"]
+for i in arr:
+    if i == "a":
+        print(f"I have {i}")
+    else:
+        print("kcb")
+        
+i=0
+while i < 10 :
+    print ("kcb is the best")
+    i+=4
+
+
+for i in range(5):
+    if 
---
+++ var.py
@@ -0,0 +1,9 @@
+m=int(input("Enter num: "))
+n=int(input("Enter number 2: "))
+print(n)
+print(m)
+z=m**2
+l=n//2
+print(z)
+print(l)
+print(int(z)+int(l))
