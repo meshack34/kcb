0891c974aadad4837ca0777a07deee7b118b37e2
Hillary Mutai <mutaihillary01@gmail.com>
2024-02-28 19:25:16 +0300

    updates

--- loop.py
+++ loop.py
@@ -11,7 +11,11 @@ i=0
 while i < 10 :
     print ("kcb is the best")
     i+=4
-
-
-for i in range(5):
-    if 
+m=int(input("Enter num: "))
+n=int(input("Enter number 2: "))
+print(n)
+print(m)
+z=m**2
+l=n//2
+print(z)
+print(l)
